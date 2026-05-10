16987510db3cd7208482f4fef34409acde666e45
Santosh Sharma <santoshsharmaofficial101@gmail.com>
2026-05-10 10:57:11 +0545

    adding facebook reel

---
+++ up.bat
@@ -0,0 +1,8 @@
+@echo off
+git add .
+git commit -m "site update"
+git push origin main
+echo ---------------------------------------
+echo Website Updated Successfully!
+echo ---------------------------------------
+pause
